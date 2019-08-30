e52353196732abbdd36ff5ba2c605aed80bf1822
Tianyi Hao <haoty0116@gmail.com>
2019-08-30 16:35:11 -0500

    update einsvd

--- ctf_ext.py
+++ ctf_ext.py
@@ -1,8 +1,18 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import string
+
 import ctf
 import numpy as np
 
+
 def name():
     return 'ctf'
+
+def cls():
+    return ctf.tensor
 
 def save_tensor_to_file(T, filename):
     np.save(filename, T.to_nparray())
@@ -29,55 +39,43 @@ def is_master_proc():
 
 def printf(*string):
     if ctf.comm().rank() == 0:
-        print(string)
+        print(*string)
 
 def tensor(shape, sp, *args):
     return ctf.tensor(shape, sp, *args)
-
+        
 def sparse_random(shape, begin, end, sp_frac):
-    tensor = ctf.tensor(shape, sp=True)
-    tensor.fill_sp_random(begin, end, sp_frac)
-    return tensor
+    return ctf.tensor(shape, sp=True).fill_sp_random(begin, end, sp_frac)
 
 def vecnorm(T):
     return ctf.vecnorm(T)
-    
+
 def list_vecnormsq(list_A):
-    l = [i**2 for i in list_A]
     s = 0
-    for i in range(len(l)):
-        s+=ctf.sum(l[i])
+    for t in [i**2 for i in list_A]:
+        s += ctf.sum(t)
     return s
     
 def list_vecnorm(list_A):
-    l = [i**2 for i in list_A]
-    s = 0
-    for i in range(len(l)):
-        s+=ctf.sum(l[i])
-        
-    return s**0.5
-    
+    return list_vecnormsq(list_A)**0.5
     
 def mult_lists(list_A,list_B):
-    l=[A*B for (A,B) in zip(list_A,list_B)]
-    s= 0
-    for i in range(len(l)):
-        s+=ctf.sum(l[i])
-        
+    s = 0
+    for t in [A * B for (A, B) in zip(list_A, list_B)]:
+        s += ctf.sum(t)
     return s
     
-
 def norm(v):
     return v.norm2() 
 
-def dot(A,B):
-    return ctf.dot(A,B)
+def dot(A, B):
+    return ctf.dot(A, B)
 
-def svd(A,r=None):
-    return ctf.svd(A,r)
+def svd(A, r=None):
+    return ctf.svd(A, r)
 
-def svd_rand(A,r):
-    return ctf.svd_rand(A,r)
+def svd_rand(A, r):
+    return ctf.svd_rand(A, r)
 
 def cholesky(A):
     return ctf.cholesky(A)
@@ -86,17 +84,18 @@ def solve_tri(A, B, lower=True, from_left=False, transp_L=False):
     return ctf.solve_tri(A, B, lower, from_left, transp_L)
 
 def einsum(string, *args):
-    if "..." in string:
-        left = string.split(",")
-        left[-1], right = left[-1].split("->")
-        symbols = "".join(list(set([chr(i) for i in range(48, 127)]) - set(string.replace(".", "").replace(",", "").replace("->", ""))))
+    # doesn't check for invalid use of ellipses
+    if '...' in string:
+        left = string.split(',')
+        left[-1], right = left[-1].split('->')
+        symbols = ''.join(list(set(chr(i) for i in range(48, 127)) - set(string.replace('.', '').replace(',', '').replace('->', ''))))
         symbol_idx = 0
         for i, (s, tsr) in enumerate(zip(left, args)):
-            num_missing = tsr.ndim - len(s.replace("...", ""))
+            num_missing = tsr.ndim - len(s.replace('...', ''))
             left[i] = s.replace("...", symbols[symbol_idx:symbol_idx+num_missing])
             symbol_idx += num_missing
-        right = right.replace("...", symbols[:symbol_idx])
-        string = ",".join(left) + "->" + right
+        right = right.replace('...', symbols[:symbol_idx])
+        string = ','.join(left) + '->' + right
 
     return ctf.einsum(string, *args)
 
@@ -114,12 +113,12 @@ def random(shape):
 
 def seed(seed):
     return ctf.random.seed(seed)
+
+def list_add(list_A, list_B):
+    return [A + B for (A, B) in zip(list_A, list_B)]
     
-def list_add(list_A,list_B):
-    return [A+B for (A,B) in zip(list_A,list_B)]
-    
-def scalar_mul(sclr,list_A):
-    return [sclr*A for A in list_A]
+def scalar_mul(sclr, list_A):
+    return [sclr * A for A in list_A]
 
 def speye(*args):
     return ctf.speye(*args)
@@ -136,23 +135,85 @@ def argmax(A, axis=0):
 def asarray(T):
     return ctf.astensor(T)
     
+def reshape(A, shape, order='F'):
+    return ctf.reshape(A, shape, order)
 
-def reshape(A,shape,order='F'):
-    return ctf.reshape(A,shape,order)
+def tensordot(A, B, axes):
+    return ctf.tensordot(A, B, axes)
+    
+def astensor(A):
+    return ctf.astensor(A)
 
-def einsvd(einstr, A, r=None, transpose=True, compute_uv=True, full_matrices=True, mult_sv=False):
-    str_a, str_uv = einstr.split("->")
-    str_u, str_v = str_uv.split(",")
-    U, S, Vh = A.i(str_a).svd(str_u, str_v, rank=r)
-    if not compute_uv:
-        return S
-    if mult_sv:
-        char_i = list(set(str_v) - set(str_a))[0]
-        char_s = list(set(str_a) - set(str_v))[0]
-        Vh = ctf.einsum(char_s + char_i + "," + str_v + "->" + str_v.replace(char_i, char_s), ctf.diag(S), Vh)
-    if not transpose:
-        Vh = Vh.T
-    return U, S, Vh
+def einsvd(einstr, A, rank=None, threshold=None, size_limit=None, criterion=None, mult_s=True):
+    """
+    Perform Singular Value Decomposition according to the specified Einstein notation string. 
+    Will always preserve at least one singular value during truncation.
+
+    Parameters
+    ----------
+    einstr: str
+        A string of Einstein notations in the form of 'idxofA->idxofU,idxofV'. There must be one and only one contraction index.
+
+    A: tensor like
+        The tensor to be decomposed. Should be of order 2 or more.
+
+    rank: int or None, optional
+        The minimum number of singular values/vectors to preserve. Will influence the actual truncation rank.
+
+    threshold: float or None, optional
+        The value used with criterion to decide the cutoff. Will influence the actual truncation rank.
+
+    size_limit: int or tuple or None, optional
+        The size limit(s) for both U and V (when specified as a int) or U and V respectively (when specified as a tuple).
+        Will influence the actual truncation rank.
+
+    criterion: int or None, optional
+        The norm to be used together with threshold to decide the cutoff. Will influence the actual truncation rank.
+        When being left as None, the threshold is treated as the plain cutoff value.
+        Otherwise, cutoff rank is the largest int satisfies: threshold * norm(s) > norm(s[rank:]).
+
+    mult_s: bool, optional
+        Whether or not to multiply U and V by S**0.5 to decompose A into two tensors instead of three. True by default.
+    """
+    str_a, str_uv = einstr.replace(' ', '').split('->')
+    str_u, str_v = str_uv.split(',')
+    char_i = list(set(str_v) - set(str_a))[0]
+    shape_u = np.prod([A.shape[str_a.find(c)] for c in str_u if c != char_i])
+    shape_v = np.prod([A.shape[str_a.find(c)] for c in str_v if c != char_i])
+
+    rank = rank or min(shape_u, shape_v)
+
+    if size_limit is not None:
+        if isinstance(size_limit, int):
+            size_limit = (size_limit, size_limit)
+        if size_limit[0] is not None:
+            rank = min(rank, int(size_limit[0] / shape_u))
+        if size_limit[1] is not None:
+            rank = min(rank, int(size_limit[1] / shape_v))
+
+    if threshold is None or criterion is None:
+        u, s, vh = A.i(str_a).svd(str_u, str_v, rank, threshold, use_svd_rand=True)
+    else:
+        u, s, vh = A.i(str_a).svd(str_u, str_v)
+        threshold = threshold * ctf.norm(s, criterion)
+        # will always preserve at least one singular value
+        for i in range(rank, 0, -1):
+            if ctf.norm(s[i:], criterion) >= threshold:
+                rank = i
+                break;
+        if rank < s.size:
+            u = u[tuple(slice(None) for i in range(str_u.find(char_i))) + (slice(0, rank),)]
+            s = s[:rank]
+            vh = vh[tuple(slice(None) for i in range(str_v.find(char_i))) + (slice(0, rank),)]
+
+    if mult_s:
+        char_s = list(set(string.ascii_letters) - set(str_v))[0]
+        sqrtS = ctf.diag(s ** 0.5)
+        vh = ctf.einsum(char_s + char_i + ',' + str_v + '->' + str_v.replace(char_i, char_s), sqrtS, vh)
+        char_s = list(set(string.ascii_letters) - set(str_u))[0]
+        u = ctf.einsum(str_u + ',' + char_s + char_i + '->' + str_u.replace(char_i, char_s), u, sqrtS)
+
+    return u, s, vh
 
 def squeeze(A):
     return A.reshape([s for s in A.shape if s != 1])
--- numpy_ext.py
+++ numpy_ext.py
@@ -1,9 +1,17 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
 import numpy.linalg as la
 import scipy.linalg as sla
 
+
 def name():
     return 'numpy'
+
+def cls():
+    return np.ndarray
 
 def save_tensor_to_file(T, filename):
     np.save(filename, T)
@@ -32,35 +40,29 @@ def is_master_proc():
     return True
 
 def printf(*string):
-    print(string)
+    print(*string)
 
 def tensor(shape, sp, *args2):
     return np.ndarray(shape, *args2)
 
-
-def list_add(list_A,list_B):
-    return [A+B for (A,B) in zip(list_A,list_B)]
+def list_add(list_A, list_B):
+    return [A + B for (A, B) in zip(list_A, list_B)]
 
 def scalar_mul(sclr,list_A):
-    return [sclr*A for A in list_A]
+    return [sclr * A for A in list_A]
 
 def mult_lists(list_A,list_B):
-    l=[A*B for (A,B) in zip(list_A,list_B)]
-
-    return np.sum(np.sum(l))
+    return np.sum(np.sum([A * B for (A, B) in zip(list_A, list_B)]))
 
 def list_vecnormsq(list_A):
-    l = [i**2 for i in list_A]
-    return np.sum(l)
+    return np.sum([i**2 for i in list_A])
 
 def list_vecnorm(list_A):
-    l = [i**2 for i in list_A]
-
-    return np.sqrt(np.sum(l))
+    return np.sqrt(np.sum([i**2 for i in list_A]))
 
 def sparse_random(shape, begin, end, sp_frac):
-    tensor = np.random.random(shape)*(end-begin) + begin
-    mask = np.random.random(shape)<sp_frac
+    tensor = np.random.random(shape) * (end - begin) + begin
+    mask = np.random.random(shape) < sp_frac
     tensor = tensor * mask
     return tensor
 
@@ -70,8 +72,8 @@ def vecnorm(T):
 def norm(v):
     return la.norm(v)
 
-def dot(A,B):
-    return np.dot(A,B)
+def dot(A, B):
+    return np.dot(A, B)
 
 def eigvalh(A):
     return la.eigvalh(A)
@@ -79,33 +81,35 @@ def eigvalh(A):
 def eigvalsh(A):
     return la.eigvalsh(A)
 
-def svd(A,r=None):
-    U,s,VT = la.svd(A,full_matrices=False)
+def svd(A, r=None):
+    U, s, VT = la.svd(A, full_matrices=False)
     if r is not None:
         U = U[:,:r]
         s = s[:r]
         VT = VT[:r,:]
-    return U,s,VT
+    return U, s, VT
 
-def svd_rand(A,r=None):
-    return svd(A,r)
+def svd_rand(A, r=None):
+    return svd(A, r)
 
 def cholesky(A):
     return la.cholesky(A)
 
 def solve_tri(A, B, lower=True, from_left=True, transp_L=False):
     if not from_left:
-        B = B.T
-        A = A.T
-        llower = not lower
-        X = sla.solve_triangular(A, B, trans=transp_L, lower=llower)
-        return X.T
+        return sla.solve_triangular(A.T, B.T, trans=transp_L, lower=not lower).T
     else:
-        return sla.solve_triangular(A, B, trans=transp_L,lower=lower)
+        return sla.solve_triangular(A, B, trans=transp_L, lower=lower)
 
 def einsum(string, *args):
-    out = np.einsum(string, *args)
-    return out
+    return np.einsum(string, *args)
+
+#def einsum(string, *args, out=None):
+#    if out is None:
+#        return np.einsum(string, *args)
+#    else:
+#        out = np.einsum(string, *args)
+#        return out
 
 def ones(shape):
     return np.ones(shape)
@@ -140,73 +144,90 @@ def argmax(A, axis=0):
 def qr(A):
     return la.qr(A)
 
-def reshape(A,shape,order='F'):
-    return np.reshape(A,shape,order)
+def reshape(A, shape, order='F'):
+    return np.reshape(A, shape, order)
 
-def einsvd(operand, tns, r=None, transpose=True, compute_uv=True, full_matrices=True, mult_sv=False):
-    ''' compute SVD of tns
+def tensordot(A, B, axes):
+    return np.tensordot(A, B, axes)
 
-    oprand (str): in form 'src -> tgta, tgtb'
-    tns (ndarray): tensor to be decomposed
-    transpose (bool): True iff VT(H) is required instead of V
-    compute_uv, full_matrices (bool): see numpy.linalg.svd
+def astensor(A):
+    # if isinstance(A, np.ndarray):
+    #     return A
+    # if np.isscalar(A):
+    #     return np.asarray(A)
+    if hasattr(A, 'to_nparray'):
+        return A.to_nparray()
+    return np.asarray(A)
 
-    REQUIRE: only one contracted index
-    '''
+def einsvd(einstr, A, rank=None, threshold=None, size_limit=None, criterion=None, mult_s=True):
+    """
+    Perform Singular Value Decomposition according to the specified Einstein notation string. 
+    Will always preserve at least one singular value during truncation.
 
-    src, _, tgt = operand.replace(' ', '').partition('->')
-    tgta, _, tgtb = tgt.partition(',')
+    Parameters
+    ----------
+    einstr: str
+        A string of Einstein notations in the form of 'idxofA->idxofU,idxofV'. There must be one and only one contraction index.
 
-    # transpose and reshape to the matrix to be SVD'd
-    tgt_idx = set(tgta).union(set(tgtb))
-    contract_idx = str(list(tgt_idx.difference(set(src)))[0])
-    new_idx = (tgta + tgtb).replace(contract_idx, '')
-    trsped = np.einsum(src + '->' + new_idx, tns)
+    A: tensor like
+        The tensor to be decomposed. Should be of order 2 or more.
 
-    # do svd
-    shape = tns.shape
-    letter2size = {}
-    for i in range(len(src)):
-        letter2size[src[i]] = shape[i]
-    col_idx = tgtb.replace(contract_idx, '')
-    ncol = 1
-    for letter in col_idx:
-        ncol *= letter2size[letter]
-    mat = trsped.reshape((-1, ncol))
-    if not compute_uv:
-        return la.svd(mat, compute_uv=False)
+    rank: int or None, optional
+        The minimum number of singular values/vectors to preserve. Will influence the actual truncation rank.
 
-    # if u, v are needed
-    u, s, vh = la.svd(mat, full_matrices=full_matrices)
+    threshold: float or None, optional
+        The value used with criterion to decide the cutoff. Will influence the actual truncation rank.
 
-    if r != None and r < len(s):
-        u = u[:,:r]
-        s = s[:r]
-        vh = vh[:r,:]
-    if mult_sv:
-        vh = np.dot(np.diag(s),vh)
+    size_limit: int or tuple or None, optional
+        The size limit(s) for both U and V (when specified as a int) or U and V respectively (when specified as a tuple).
+        Will influence the actual truncation rank.
 
-    # reshape u, v into shape (..., contract) and (contract, ...)
-    row_idx = tgta.replace(contract_idx, '')
-    shapeA = []
-    shapeB = [-1]
-    for letter in row_idx:
-        shapeA.append(letter2size[letter])
-    for letter in col_idx:
-        shapeB.append(letter2size[letter])
-    shapeA.append(-1)
-    u = u.reshape(shapeA)
-    vh = vh.reshape(shapeB)
+    criterion: int or None, optional
+        The norm to be used together with threshold to decide the cutoff. Will influence the actual truncation rank.
+        When being left as None, the threshold is treated as the plain cutoff value.
+        Otherwise, cutoff rank is the largest int satisfies: threshold * norm(s) > norm(s[rank:]).
 
-    # transpose u and vh into tgta and tgtb
-    preA = tgta.replace(contract_idx, '') + contract_idx
-    preB = contract_idx + tgtb.replace(contract_idx, '')
-    u = np.einsum(preA + '->' + tgta, u)
-    vh = np.einsum(preB + '->' + tgtb, vh)
+    mult_s: bool, optional
+        Whether or not to multiply U and V by S**0.5 to decompose A into two tensors instead of three. True by default.
+    """
+    str_a, str_uv = einstr.replace(' ', '').split('->')
+    str_u, str_v = str_uv.split(',')
+    char_i = list(set(str_v) - set(str_a))[0]
 
-    # return
-    if not transpose:
-        vh = np.conj(vh.T)
+    # transpose and reshape A into u.size * v.size and do svd
+    u, s, vh = la.svd(np.einsum(str_a + '->' + (str_u + str_v).replace(char_i, ''), A).reshape((-1, np.prod([A.shape[str_a.find(c)] for c in str_v if c != char_i], dtype=int))), full_matrices=False)
+    rank = rank or len(s)
+
+    if size_limit is not None:
+        if isinstance(size_limit, int):
+            size_limit = (size_limit, size_limit)
+        rank = min(rank, int(size_limit[0] / u.shape[0]))
+        rank = min(rank, int(size_limit[1] / vh.shape[1]))
+
+    if threshold is not None:
+        # will always preserve at least one singular value
+        if criterion is None:
+            rank = 1 if threshold > s[0] else min((rank, len(s) - np.searchsorted(s[::-1], threshold)))
+        else:
+            threshold = threshold * la.norm(s, criterion)
+            for i in range(rank, 0, -1):
+                if la.norm(s[i:], criterion) >= threshold:
+                    rank = i
+                    break;
+
+    if rank < len(s):
+        u = u[:,:rank]
+        s = s[:rank]
+        vh = vh[:rank]
+
+    if mult_s:
+        sqrtS = np.diag(s ** 0.5)
+        u = np.dot(u, sqrtS)
+        vh = np.dot(sqrtS, vh)
+
+    # reshape and transpose u and vh into tgta and tgtb
+    u = np.einsum(str_u.replace(char_i, '') + char_i + '->' + str_u, u.reshape([A.shape[str_a.find(c)] for c in str_u if c != char_i] + [-1]))
+    vh = np.einsum(char_i + str_v.replace(char_i, '') + '->' + str_v, vh.reshape([-1] + [A.shape[str_a.find(c)] for c in str_v if c != char_i]))
     return u, s, vh
 
 def squeeze(A):
